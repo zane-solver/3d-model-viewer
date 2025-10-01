b79b88fb42796c869d2cdfca1e043229ed7d4efa
Jaime Villegas <jaimevillegas296@gmail.com>
2025-09-30 20:43:59 -0500

    Add the ability to load models by the url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { MainLayout } from '@/components/layout/MainLayout'
 import { Sidebar } from '@/components/layout/Sidebar'
 import { FileUploadDialog } from '@/components/FileUploadDialog'
 import { ViewerContainer } from '@/components/viewer/ViewerContainer'
+import { useViewerStore } from './store/viewerStore'
+import defaultModelUrl from '@/assets/default.obj?url'
+
+// Use Vite's import.meta.glob to get all .obj files as URLs
+const modelUrls = import.meta.glob('@/assets/*.obj', { as: 'url', eager: true })
 
 function App() {
   const [uploadDialogOpen, setUploadDialogOpen] = useState(false)
+  const { setCurrentModel } = useViewerStore()
+
+  useEffect(() => {
+    const loadModelFromUrl = () => {
+      const params = new URLSearchParams(window.location.search)
+      const modelName = params.get('model')
+      let modelUrl = defaultModelUrl
+      let name = 'default.obj'
+
+      if (modelName) {
+        // The keys in modelUrls will be like '/src/assets/model.obj'
+        const modelKey = `/src/assets/${modelName}`
+
+        if (modelUrls[modelKey]) {
+          modelUrl = modelUrls[modelKey]
+          name = modelName
+        } else {
+          console.warn(`Model not found: ${modelName}. Loading default model.`)
+        }
+      }
+
+      const model = {
+        id: name,
+        name: name,
+        url: modelUrl,
+        file: new File([], name),
+      }
+      setCurrentModel(model)
+    }
+
+    loadModelFromUrl()
+  }, [setCurrentModel])
 
   // Camera control functions
   const handleResetView = () => {
